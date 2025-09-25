0a6885c4996e8fa5eb21a4a2414d686fbe87b33b
Wasim Akram <128415984+wasim-1@users.noreply.github.com>
2025-09-25 12:37:18 +0000

    Add files

--- strings.js
+++ strings.js
@@ -5,3 +5,10 @@ let str3 ='Float on the ocean';
 console.log(str1);
 console.log(str2);
 console.log(str3);
+
+console.log("--------------------------------------------------");
+let  str4 = "This is the first line\nAnd this is the second"
+console.log(str4);
+console.log("--------------------------------------------------");
+let  concatStr = "con" + "cat" + "e" + "nate";
+console.log(concatStr);
