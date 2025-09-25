e0335bd80b5ebff45990863d7a139499dbc92365
Wasim Akram <128415984+wasim-1@users.noreply.github.com>
2025-09-25 13:13:48 +0000

    Add files

---
+++ Datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS files as strict mode
+
+console.log(3 
+    + 3)
+console.log("Hello World")
+
+let name = "Wasim Akram";
+let age = 21;
+let isLoggedIn = false;
+
+console.log(name);
+console.log(age);
+console.log(isLoggedIn);
+console.log("");
+
+// number => 2 to power 53
+// bigint
+// string => '', "", ``
+// boolean => true, false
+// null => standalone value
+// undefined => standalone value
+// symbol => unique
+
+// object
+
+console.log(typeof 'Wasim Akram'); // string
+console.log(typeof null); // object (bug)
+console.log(typeof undefined); // undefined
+console.log(typeof 21); // number
+console.log(typeof NaN); // number
