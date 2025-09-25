2a8c04fe7e4587bb28f58ee7cdff649d13c59714
Wasim Akram <128415984+wasim-1@users.noreply.github.com>
2025-09-25 12:58:36 +0000

    Add files

---
+++ variables.js
@@ -0,0 +1,17 @@
+// Variable declaration and initialization
+let gitHubUserName = "Wasim-1";
+let LinkedInUserName = "wasimakram17";
+console.log(gitHubUserName);
+console.log(LinkedInUserName);
+console.log("");
+
+const accountHolder = "Wasim Akram";
+const accountNumber = 20349375781;
+const bankName = "State Bank of India";
+const IFSC = "SBIN0002950";
+console.log(accountHolder);
+console.log(accountNumber);
+console.log(bankName);
+console.log(IFSC);
+console.log("");
+var city = "Jainagar";
